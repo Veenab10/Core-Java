a33f6c6fc4a89e4ef672e06be6cd23f6d10731ee
veenab10 <veenab.xworkz@gmail.com>
2024-01-30 09:43:02 +0530

    Data Types

---
+++ DataTypes/src/com/xworkz/DataTypes/RunnerClass/BrushRunner.java
@@ -0,0 +1,161 @@
+package com.xworkz.DataTypes.RunnerClass;
+
+import com.xworkz.DataTypes.exampleofdatatype.Brush;
+
+public class BrushRunner {
+	  public static void main(String[] args) {
+
+	        Brush toothbrush = new Brush();
+
+	        System.out.println(toothbrush.brandName);
+	        System.out.println(toothbrush.productName);
+
+	        System.out.println(toothbrush.bristleType);
+	        System.out.println(toothbrush.handleMaterial);
+
+	        System.out.println(toothbrush.color);
+	        System.out.println(toothbrush.price);
+
+	        System.out.println(toothbrush.manufacturer);
+	        System.out.println(toothbrush.madeIn);
+
+	        System.out.println(toothbrush.productionDate);
+	        System.out.println(toothbrush.expiryDate);
+
+	        System.out.println(toothbrush.handleShape);
+	        System.out.println(toothbrush.hasTongueCleaner);
+
+	        System.out.println(toothbrush.isElectric);
+	        System.out.println(toothbrush.gripMaterial);
+
+	        System.out.println(toothbrush.isWaterproof);
+	        System.out.println(toothbrush.recommendedAgeGroup);
+
+	        System.out.println(toothbrush.hasReplacementIndicator);
+	        System.out.println(toothbrush.bristleColor);
+
+	        System.out.println(toothbrush.bristleCount);
+	        System.out.println(toothbrush.packagingType);
+
+	        System.out.println(toothbrush.packagingColor);
+	        System.out.println(toothbrush.usageInstructions);
+
+	        System.out.println(toothbrush.isDentistRecommended);
+	        System.out.println(toothbrush.brandAmbassador);
+
+	        System.out.println(toothbrush.isEnvironmentallyFriendly);
+	        System.out.println(toothbrush.warrantyPeriod);
+
+	        System.out.println(toothbrush.isBatteryOperated);
+	        System.out.println(toothbrush.hasTravelCase);
+
+	        System.out.println(toothbrush.brandOwner);
+	        System.out.println(toothbrush.brandOrigin);
+
+	        System.out.println(toothbrush.handleLength);
+	        System.out.println(toothbrush.isAntiBacterial);
+
+	        System.out.println(toothbrush.productFormulation);
+	        System.out.println(toothbrush.isRecyclable);
+
+	        System.out.println(toothbrush.hasGumProtection);
+	        System.out.println(toothbrush.isOrthodonticFriendly);
+
+	        System.out.println(toothbrush.handleTexture);
+	        System.out.println(toothbrush.handleDesign);
+
+	        System.out.println(toothbrush.handlePattern);
+	        System.out.println(toothbrush.isCharcoalInfused);
+
+	        System.out.println(toothbrush.bristlePattern);
+	        System.out.println(toothbrush.bristleHeight);
+
+	        System.out.println(toothbrush.isSensitiveTeethFriendly);
+	        System.out.println(toothbrush.isApprovedByADA);
+
+	        System.out.println(toothbrush.website);
+	        
+	        System.out.println(toothbrush.hasBluetoothConnectivity);
+	        
+	        System.out.println(toothbrush.isSmartToothbrush);
+	        
+	        System.out.println(toothbrush.pressureSensorSensitivity);
+	        
+	        System.out.println(toothbrush.connectivityProtocol);
+	        
+	        System.out.println(toothbrush.hasUVSanitizer);
+	        
+	        System.out.println(toothbrush.isDisposable);
+	        
+	        System.out.println(toothbrush.hasTimer);
+	        
+	        System.out.println(toothbrush.timerInterval);
+	        
+	        System.out.println(toothbrush.hasPressureSensor);
+	        
+	        System.out.println(toothbrush.isFlexible);
+	        
+	        System.out.println(toothbrush.additionalFeatures);
+	        
+	        System.out.println(toothbrush.isRechargeable);
+	        
+	        System.out.println(toothbrush.batteryType);
+	        
+	        System.out.println(toothbrush.hasAppIntegration);
+	        
+	        System.out.println(toothbrush.hasSonicTechnology);
+	        
+	        System.out.println(toothbrush.sonicVibrationsPerMinute);
+	        
+	        System.out.println(toothbrush.hasAngledBristles);
+	        
+	        System.out.println(toothbrush.handleGripTexture);
+	        
+	        System.out.println(toothbrush.warrantyDetails);
+	        
+	        System.out.println(toothbrush.hasBristleIndicator);
+	        
+	        System.out.println(toothbrush.replacementIndicatorType);
+	        
+	        System.out.println(toothbrush.specialEdition);
+	        
+	        System.out.println(toothbrush.isBambooHandle);
+	        
+	        System.out.println(toothbrush.hasCharcoalInfusion);
+	        
+	        System.out.println(toothbrush.sustainabilityCertification);
+	        
+	        System.out.println(toothbrush.recyclabilityDetails);
+	        
+	        System.out.println(toothbrush.isAntiPlaque);
+	        
+	        System.out.println(toothbrush.plaqueRemovalTechnology);
+	        
+	        System.out.println(toothbrush.isADAApproved);
+	        
+	        System.out.println(toothbrush.ADAApprovalDate);
+	        
+	        System.out.println(toothbrush.hasTongueScraper);
+	        
+	        System.out.println(toothbrush.isBPAFree);
+	        
+	        System.out.println(toothbrush.isPhthalateFree);
+	        
+	        System.out.println(toothbrush.isLatexFree);
+	        
+	        System.out.println(toothbrush.hasTravelCap);
+	        System.out.println(toothbrush.hasNaturalIngredients);
+	        System.out.println(toothbrush.naturalIngredientList);
+	        System.out.println(toothbrush.manufacturingCountry);
+	        System.out.println(toothbrush.countryOfOrigin);
+	        System.out.println(toothbrush.isClinicallyProven);
+	        System.out.println(toothbrush.clinicalStudyDetails);
+	        System.out.println(toothbrush.isFDAApproved);
+	        System.out.println(toothbrush.isCEMarked);
+	        System.out.println(toothbrush.CEApprovalDate);
+	        System.out.println(toothbrush.dimensions);
+	        System.out.println(toothbrush.hasSoftGripHandle);
+	    }
+	}
+
+
