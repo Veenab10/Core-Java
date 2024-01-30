ff392672e68b6925eb4f8da4594ca9e4db49751c
veenab10 <veenab.xworkz@gmail.com>
2024-01-30 10:10:52 +0530

    Data Types

---
+++ DataTypes/src/com/xworkz/DataTypes/RunnerClass/KnifeRunner.java
@@ -0,0 +1,174 @@
+package com.xworkz.DataTypes.RunnerClass;
+
+public class KnifeRunner {
+	public static void main(String args[])
+	{
+	   Knife knife = new Knife();	
+	
+	    knife.name="Inferno";
+	    knife.type="Kitchen Knife";
+	    knife.bladeMaterial="Stainless";
+	    knife.handleMaterial="Wood";
+	    knife.bladeLength=4;
+	    knife.overallLength=8;
+	    knife.bladeShape="Drop";
+	    knife.grindType="Hollow";
+	    knife.edgeType="Plain";
+	    knife.tangType="Full";
+	    knife.HandleDesign="Ergonomic";
+	    knife.weight="Lightweight";
+	    knife.balance="Handle-heavy";
+	    knife.spineThickness="Thick";
+	    knife.deploymentMechanism="Manual";
+	    knife.lockingMechanism="Liner";
+	    knife.pocketClip="Recersible";
+	    knife.sheathMaterial="Kydex";
+	    knife.retentionSystem="snap";
+	    knife.openingMethod="Thumb";
+    	knife.bladeCoating="DLC";
+	    knife.rustResistance="coated";
+	    knife.corrosionResistance="Rust";
+	    knife.handleColor="Black";
+	    knife.lanyardHole=true;
+	    knife.scales="Textured";
+	    knife.jimping="Thumb";
+	    knife.pommelType="glass";
+	    knife.bladeThickness="thick";
+	    knife.spineDesign="swedgr";
+	    knife.bladeSteelHardness=58;
+	    knife.ambidextrousFeatures="studs";
+	    knife.fingerGrooves=false;
+	    knife.handleLength="Short";
+	    knife.bladeFinish="stonewash";
+	    knife.ricasso="minimal";
+	    knife.bladeSerrationType="partial";
+	    knife.tacticalFeatures="seatbelt";
+	    knife.rearLocking="back";
+	    knife.gripTexture="checkerd";
+	    knife.fullersGrooves=false;
+	    knife.fingerGuard="finger";
+	    knife.bladeProfile="spear";
+	    knife.bladeWidth="wide";
+	    knife.handleErgonomics="comfort";
+	    knife.mosaicPins="pins";
+	    knife.bladeGeometry="flat";
+	    knife.razorSharpness="razor";
+		knife.rustproofingTreatment="Teflon";
+		knife.bladeBelly="Custom";
+		knife.bladeAngle=154;
+	    knife.customizationOptions="HRC";
+		knife.bladeSteelComposition=3;
+		knife.thumbRamp="easy";
+		knife.easeofSharpening="local";
+		knife.legalBladeLength="pivot";
+		knife.tensionAdjustment="overtravel";
+		knife.lockbarStabilizer="quick";
+		knife.assistedOpeningSpeed="thick";
+		knife.bladeBackThickness="edc";
+		knife.purpose="backspare";
+		knife.backspacerDesign="wood";											
+		knife.handleInlays="light";												
+		knife.gentlemanFolderFeatures="thumb";											
+		knife.detentStrength="easy";														
+		knife.bladeHoles="slim";															
+		knife.lockbarAccess="matt";														
+		knife.handleThickness="rotate";														
+		knife.nonReflectiveFinish="grip";													
+		knife.thumbDisc="up";																
+		knife.handleContours="indiamade";													
+		knife.trailingPoint="treat";														
+		knife.bladeMaterialOrigin="ceramic";											
+		knife.heatTreatmentMethod="rust";											
+		knife.ceramicBladeOption="straight";											
+		
+		
+		System.out.println(knife.name);
+		System.out.println(knife.type);
+		System.out.println(knife.bladeMaterial);
+		System.out.println(knife.handleMaterial);
+		System.out.println(knife.bladeLength);
+		System.out.println(knife.overallLength);
+		System.out.println(knife.bladeShape);
+		System.out.println(knife.grindType);
+		System.out.println(knife.edgeType);
+		System.out.println(knife.tangType);
+		System.out.println(knife.HandleDesign);
+		
+		System.out.println(knife.weight);
+		System.out.println(knife.balance);
+		System.out.println(knife.spineThickness);
+		System.out.println(knife.deploymentMechanism);
+		System.out.println(knife.lockingMechanism);
+		System.out.println(knife.pocketClip);
+		System.out.println(knife.sheathMaterial);
+		System.out.println(knife.retentionSystem);
+		System.out.println(knife.openingMethod);
+		System.out.println(knife.bladeCoating);
+		
+		System.out.println(knife.rustResistance);
+		System.out.println(knife.corrosionResistance);
+		System.out.println(knife.handleColor);
+		System.out.println(knife.lanyardHole);
+		System.out.println(knife.scales);
+		System.out.println(knife.jimping);
+		System.out.println(knife.pommelType);
+		System.out.println(knife.bladeThickness);
+		System.out.println(knife.spineDesign);
+		System.out.println(knife.bladeSteelHardness);
+		
+		System.out.println(knife.ambidextrousFeatures);
+		System.out.println(knife.fingerGrooves);
+		System.out.println(knife.handleLength);
+		System.out.println(knife.bladeFinish);
+		System.out.println(knife.ricasso);
+		System.out.println(knife.bladeSerrationType);
+		System.out.println(knife.tacticalFeatures);
+		System.out.println(knife.rearLocking);
+		System.out.println(knife.gripTexture);
+		
+		System.out.println(knife.fullersGrooves);
+		System.out.println(knife.fingerGuard);
+		System.out.println(knife.bladeProfile);
+		System.out.println(knife.bladeWidth);
+		System.out.println(knife.handleErgonomics);
+		System.out.println(knife.mosaicPins);
+		System.out.println(knife.bladeGeometry);
+		System.out.println(knife.razorSharpness);
+		System.out.println(knife.rustproofingTreatment);
+		System.out.println(knife.bladeBelly);
+		
+		System.out.println(knife.bladeAngle);
+		System.out.println(knife.customizationOptions);
+		System.out.println(knife.bladeSteelComposition);
+		System.out.println(knife.thumbRamp);
+		System.out.println(knife.easeofSharpening);
+		System.out.println(knife.legalBladeLength);
+		System.out.println(knife.tensionAdjustment);
+		System.out.println(knife.lockbarStabilizer);
+		System.out.println(knife.assistedOpeningSpeed);
+		System.out.println(knife.bladeBackThickness);
+		
+		System.out.println(knife.purpose);
+		System.out.println(knife.backspacerDesign);
+		System.out.println(knife.handleInlays);
+		System.out.println(knife.gentlemanFolderFeatures);
+		System.out.println(knife.detentStrength);
+		System.out.println(knife.bladeHoles);
+		System.out.println(knife.lockbarAccess);
+		System.out.println(knife.handleThickness);
+		System.out.println(knife.nonReflectiveFinish);
+		System.out.println(knife.thumbDisc);
+		
+		System.out.println(knife.handleContours);
+		System.out.println(knife.trailingPoint);
+		System.out.println(knife.bladeMaterialOrigin);
+		System.out.println(knife.heatTreatmentMethod);
+		System.out.println(knife.ceramicBladeOption);
+		System.out.println(knife.rustEradicationFeatures);
+		System.out.println(knife.handleShape);
+		System.out.println(knife.specializedCoating);
+		System.out.println(knife.warranty);
+		System.out.println(knife.pivotType);
+   }
+}
+
---
+++ DataTypes/src/com/xworkz/DataTypes/exampleofdatatype/Knife.java
@@ -0,0 +1,99 @@
+package com.xworkz.DataTypes.exampleofdatatype;
+
+public class Knife {
+
+	public String name;
+	public String type;
+	public String bladeMaterial;
+	public String handleMaterial;
+	public int bladeLength;
+	public int overallLength;
+	public String bladeShape;
+	public String grindType;
+	public String edgeType;
+	public String tangType;
+	
+	public String HandleDesign;
+	public String weight;
+	public String balance;
+	public String spineThickness;
+	public String deploymentMechanism;
+	public String lockingMechanism;
+	public String pocketClip;
+	public String sheathMaterial;
+	public String retentionSystem;
+	public String openingMethod;
+	
+	public String bladeCoating;
+	public String rustResistance;
+	public String corrosionResistance;
+	public String handleColor;
+	public boolean lanyardHole;
+	public String scales;
+	public String jimping;
+	public String pommelType;
+	public String bladeThickness;
+	public String spineDesign;
+	
+	public int bladeSteelHardness;
+	public String ambidextrousFeatures;
+	public boolean fingerGrooves;
+	public String handleLength;
+	public String bladeFinish;
+	public String ricasso;
+	public String bladeSerrationType;
+	public String tacticalFeatures;
+	public String rearLocking;
+	public String gripTexture;
+	
+	public boolean fullersGrooves;
+	public String fingerGuard;
+	public String bladeProfile;
+	public String bladeWidth;
+	public String handleErgonomics;
+	public String mosaicPins;
+	public String bladeGeometry;
+		public String razorSharpness;
+		public String rustproofingTreatment;
+		public String bladeBelly;
+	
+		public int bladeAngle;
+		public String customizationOptions;
+		public int bladeSteelComposition;
+		public String thumbRamp;
+		public String easeofSharpening;
+		public String legalBladeLength;
+		public String tensionAdjustment;
+		public String lockbarStabilizer;
+		public String assistedOpeningSpeed;
+		public String bladeBackThickness;
+		
+		public String purpose;
+		public String backspacerDesign;											
+		public String handleInlays;												
+		public String gentlemanFolderFeatures;											
+		public String detentStrength;														
+		public String bladeHoles;															
+		public String lockbarAccess;														
+		public String handleThickness;														
+		public String nonReflectiveFinish;													
+		public String thumbDisc;																
+		
+		public String handleContours;													
+		public String trailingPoint;														
+		public String bladeMaterialOrigin;											
+		public String heatTreatmentMethod;											
+		public String ceramicBladeOption;											
+		public String rustEradicationFeatures;												
+		public String handleShape;															
+		public String specializedCoating;
+		public String warranty;
+		public String pivotType;
+	
+	public Knife()
+	{
+		System.out.println("this is knife");
+	}
+}
+
+
